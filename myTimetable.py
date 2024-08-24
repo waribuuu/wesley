4d685a1d57ad6444d89e89f2f7c6b2c9eef4427a
bancrafti <swishieway@gmail.com>
2024-08-24 23:58:45 +0300

    CSV approach

--- myTimetable.py
+++ myTimetable.py
@@ -1,4 +1,35 @@
-import pandas as pd
-data = pd.read_excel('TB_Burden_Country.xlsx', index_col=[0], parse_year=[0 ])
-#data.info()
- 
+import csv
+
+file_path = "Timetable.csv"
+
+# Read the CSV file
+with open(file_path, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    data = list(reader)
+
+# Extract headers
+dates = data[1]
+times = data[2]
+
+# Combine dates and times to create multi-level columns
+columns = []
+for i in range(len(dates)):
+    if dates[i] and times[i]:
+        columns.append(f"{dates[i]} {times[i]}")
+    elif dates[i]:
+        columns.append(dates[i])
+    elif times[i]:
+        columns.append(times[i])
+    else:
+        columns.append("")
+
+# Create a list of dictionaries for rows with non-empty values
+cleaned_data = []
+for row in data[3:]:
+    row_dict = {columns[i]: row[i] for i in range(len(row)) if row[i]}
+    if row_dict:
+        cleaned_data.append(row_dict)
+
+# Print the cleaned timetable
+for row in cleaned_data:
+    print(row)
